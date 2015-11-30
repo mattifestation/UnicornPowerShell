b11b261ef2d952ca36c0ee4680f8ec9a030ec9a0
Matt Graeber <mattgraeber@gmail.com>
2015-11-30 13:46:20 -0800

    Alternative failed attempt to execute scriptblock as instrumentation callback
    Also added -CallingConvention parameter to Get-DelegateType

--- Unicorn/Unicorn.psm1
+++ Unicorn/Unicorn.psm1
@@ -127,7 +127,7 @@ $FunctionDefinitions = @(
     (func unicorn uc_reg_write ($UC_ERR) @([IntPtr], [UInt32], [Int64].MakeByRefType())),
     (func unicorn uc_reg_read ($UC_ERR) @([IntPtr], [UInt32], [Int64].MakeByRefType())),
     (func unicorn uc_emu_start ($UC_ERR) @([IntPtr], [UInt64], [UInt64], [UInt64], [UInt32])),
-    (func unicorn uc_hook_add ($UC_ERR) @([IntPtr], [IntPtr].MakeByRefType(), $UC_HOOK, [MulticastDelegate], [IntPtr], [UInt32], [UInt32]))
+    (func unicorn uc_hook_add ($UC_ERR) @([IntPtr], [IntPtr].MakeByRefType(), $UC_HOOK, [MulticastDelegate], [IntPtr], [UInt64], [UInt64]))
 )
 
 $Types = $FunctionDefinitions | Add-Win32Type -Module $Mod -Namespace 'UnicornEngine.NativeMethods'
@@ -228,69 +228,41 @@ function Assert-UCValidEmulatorSession {
     }
 }
 
-function New-ScriptBlockCallback {
-    param(
-        [parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [scriptblock]$Callback
-    )
-<#
-    .SYNOPSIS
-        Allows running ScriptBlocks via .NET async callbacks.
-
-    .DESCRIPTION
-        Allows running ScriptBlocks via .NET async callbacks. Internally this is
-        managed by converting .NET async callbacks into .NET events. This enables
-        PowerShell 2.0 to run ScriptBlocks indirectly through Register-ObjectEvent.         
-
-    .PARAMETER Callback
-        Specify a ScriptBlock to be executed in response to the callback.
-        Because the ScriptBlock is executed by the eventing subsystem, it only has
-        access to global scope. Any additional arguments to this function will be
-        passed as event MessageData.
-        
-    .EXAMPLE
-        You wish to run a scriptblock in reponse to a callback. Here is the .NET
-        method signature:
-        
-        void Bar(AsyncCallback handler, int blah)
-        
-        ps> [foo]::bar((New-ScriptBlockCallback { ... }), 42)                        
-
-    .OUTPUTS
-        A System.AsyncCallback delegate.
-#>
-    # is this type already defined?    
-    if (-not ("CallbackEventBridge" -as [type])) {
-        Add-Type @"
-            using System;
+function Get-DelegateType
+{
+    [OutputType([Type])]
+    Param
+    ( 
+        [Parameter( Position = 0)]
+        [Type[]]
+        $Parameters = (New-Object Type[](0)),
             
-            public sealed class CallbackEventBridge
-            {
-                public event AsyncCallback CallbackComplete = delegate { };
+        [Parameter( Position = 1 )]
+        [Type]
+        $ReturnType = [Void],
 
-                private CallbackEventBridge() {}
+        [System.Runtime.InteropServices.CallingConvention]
+        $CallingConvention
+    )
 
-                private void CallbackInternal(IAsyncResult result)
-                {
-                    CallbackComplete(result);
-                }
+    $Domain = [AppDomain]::CurrentDomain
+    $DynAssembly = New-Object System.Reflection.AssemblyName('ReflectedDelegate')
+    $AssemblyBuilder = $Domain.DefineDynamicAssembly($DynAssembly, [System.Reflection.Emit.AssemblyBuilderAccess]::Run)
+    $ModuleBuilder = $AssemblyBuilder.DefineDynamicModule('InMemoryModule', $false)
+    $TypeBuilder = $ModuleBuilder.DefineType('MyDelegateType', 'Class, Public, Sealed, AnsiClass, AutoClass', [System.MulticastDelegate])
 
-                public AsyncCallback Callback
-                {
-                    get { return new AsyncCallback(CallbackInternal); }
-                }
-
-                public static CallbackEventBridge Create()
-                {
-                    return new CallbackEventBridge();
-                }
-            }
-"@
+    if ($PSBoundParameters['CallingConvention']) {
+        $CallingConventionConstructor = [Runtime.InteropServices.UnmanagedFunctionPointerAttribute].GetConstructor(@([Runtime.InteropServices.CallingConvention]))
+        $ConstructorBuilder = New-Object Reflection.Emit.CustomAttributeBuilder($CallingConventionConstructor, [Object[]] @($PSBoundParameters['CallingConvention']))
+        $TypeBuilder.SetCustomAttribute($ConstructorBuilder)
     }
-    $bridge = [callbackeventbridge]::create()
-    Register-ObjectEvent -input $bridge -EventName callbackcomplete -action $callback -messagedata $args > $null
-    $bridge.callback
+
+    $ConstructorBuilder = $TypeBuilder.DefineConstructor('RTSpecialName, HideBySig, Public', [System.Reflection.CallingConventions]::Standard, $Parameters)
+    $ConstructorBuilder.SetImplementationFlags('Runtime, Managed')
+    $MethodBuilder = $TypeBuilder.DefineMethod('Invoke', 'Public, HideBySig, NewSlot, Virtual', $ReturnType, $Parameters)
+    $MethodBuilder.SetImplementationFlags('Runtime, Managed')
+        
+    Write-Output $TypeBuilder.CreateType()
 }
 #endregion
 
@@ -1329,7 +1301,7 @@ PS C:\>$Session | Remove-UCEmulatorSession
 
 Description
 -----------
-This will register and in theory, register a code hook. In practice, upon execution of the code hook, the scriptblock does not execute, access violates and crashed PowerShell.
+This will register and in theory, register a code hook. In practice, upon execution of the code hook, the scriptblock does not execute, throws the following error "There is no Runspace available to run scripts in this thread. You can provide one in the DefaultRunspace property of the System.Management.Automation.Runspaces.Runspace type. The script block you attempted to invoke was ...", and crashes PowerShell.
 
 .INPUTS
 
@@ -1344,6 +1316,7 @@ System.IntPtr
 Outputs an IntPtr representing the registered hook handle.
 #>
 
+    [CmdletBinding()]
     [OutputType([IntPtr])]
     param (
         [Parameter(Mandatory = $True, ValueFromPipeline = $True)]
@@ -1360,10 +1333,12 @@ Outputs an IntPtr representing the registered hook handle.
 
     $HookHandle = [IntPtr]::Zero
 
-    $Callback = New-ScriptBlockCallback -Callback $Action
+    $Delegate = Get-DelegateType -Parameters @([IntPtr], [UInt64], [UInt32], [IntPtr]) -ReturnType ([Int]) #-CallingConvention Cdecl
+
+    $Callback = $Action -as $Delegate
 
     $ScriptBlockPtr = [Delegate].GetField('_methodPtr', [Reflection.BindingFlags] 'NonPublic, Instance').GetValue($Callback).ToString('X16')
-    Write-Debug "Scriptblock unmanaged addr: 0x$ScriptBlockPtr"
+    Write-Verbose "Scriptblock unmanaged addr: 0x$ScriptBlockPtr"
 
     # For the purposes of debugging, you should validate that the address at $ScriptBlockPtr and $Callback are executed
 
